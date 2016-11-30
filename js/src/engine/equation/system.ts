c1805e6ae8c2feccc6563f30bec75832b4a46e82
Kristian Mandrup <kmandrup@gmail.com>
2016-11-30 17:09:36 +0100

    ts/js basic config

--- js/src/engine/equation/system.ts
+++ js/src/engine/equation/system.ts
@@ -7,25 +7,18 @@ const derivativeMap = {
   variable: DerivativeVariable,
 }
 
-class SystemOfEquations {
+export class SystemOfEquations {
   constructor() {
 
   }
   // applies the chain rule to expression x
   chainRule(const Expr x, const Expr deriv): Node {
-    // NodePtr dx=x->derivative(*this);
+    Node dx = x.derivative(this)
     // // perform some constant optimisation
-    // if (dx==zero)
-    //   return zero;
-    // else if (dx==one)
-    //   return deriv;
-    // else
-    //   return dx * deriv;
+    if (dx === zero) return zero
+    if (dx === one) return deriv
+    return dx * deriv;
   }
-
-  // SEE: TypeScript method overloading
-  // http://stackoverflow.com/questions/13212625/typescript-function-overloading
-  // https://blog.mariusschulz.com/2016/08/18/function-overloads-in-typescript
 
   // we need a single derivative method which delegates to class/factory function for each type
   derivative(const expr: any, const type: string): Node {
